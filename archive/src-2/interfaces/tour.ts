c951196ca70aa2d6ab73a9c682fafce6408acced
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2024-06-12 16:21:14 +0700

    refactor: remove unused

--- archive/src-2/interfaces/tour.ts
+++
@@ -1,8 +0,0 @@
-export interface TTour {
-  _id: string;
-  name: string;
-  duration: number;
-  difficulty: string;
-  price: number;
-  rating: number;
-}
--- archive/src-2/interfaces/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  _id: string;
-  name: string;
-  email: string;
-  role: string;
-  active: boolean;
-  photo: string;
-  password: string;
-}
